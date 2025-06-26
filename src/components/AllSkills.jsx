82a438274c1de26f158f5809e37bb7bb32cde139
dhingralakshya <lakshyadhingra26@gmail.com>
2025-06-27 00:35:40 +0530

    feat:improved skills section

--- src/components/AllSkills.jsx
+++ src/components/AllSkills.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
 function AllSkills(props) {
+  const skills = document.querySelectorAll('.skill');
+
+  skills.forEach(skill => {
+    skill.addEventListener('mouseenter', () => {
+      skills.forEach(s => {
+        if (s !== skill) s.classList.add('dimmed');
+      });
+    });
+
+    skill.addEventListener('mouseleave', () => {
+      skills.forEach(s => s.classList.remove('dimmed'));
+    });
+});
   return (
     <div class="skill">
       {props.icon}
